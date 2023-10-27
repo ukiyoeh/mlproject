3fcbbe7008b397cd9feacbd95e563f379849b22a
“[Devank <[bangadevank@gmail.com]>
2023-10-27 12:23:56 +0530

    data transformation added

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -7,12 +7,15 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+sys.path.insert(0, '..//components')
+
+from src.mlproject.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig():
-    train_data_path: str=os.path.join('artifact', 'train.csv')
-    test_data_path: str=os.path.join('artifact', 'test.csv')
-    raw_data_path: str=os.path.join('artifact', 'data.csv')
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'data.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -42,6 +45,9 @@ class DataIngestion:
             raise exception.CustomException(e, sys)
         
 if __name__=="__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
         
